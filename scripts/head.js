fea3be35d109216372798a261a95e48c9bffad3b
Eivind Sivertsen <eivind.sivertsen99@gmail.com>
2024-08-08 17:56:24 +0200

    feat: make slideshow on main page dynamic

--- scripts/head.js
+++ scripts/head.js
@@ -8,6 +8,6 @@ document.write('\
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> \
     <link rel="stylesheet" href="styles.css"> \
     <script type="text/javascript" src="data/products.js"></script> \
-    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script> \
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script> \
   </head> \
 ');
---
+++ scripts/slideshow.js
@@ -0,0 +1,27 @@
+function slideshow(products) {
+  const container = document.getElementById('slideshow');
+  let items = ``
+  products.forEach(function (product, index) {
+    if ((index % 3) == 0) items += '<div class="gallery-image fade">';
+
+    items += `
+      <div class="project-container">
+        <a href="product.html?product=${product.id}">
+          <img
+            src="${product.imageUrl}">
+          <div class="text">${product.category}</div>
+        </a>
+      </div>
+    `;
+
+    if ((index % 3) == 2) items += '</div>';
+  });
+
+  const htmlContent = `
+    ${items}
+  `;
+
+  container.innerHTML = htmlContent;
+}
+
+slideshow(products);
