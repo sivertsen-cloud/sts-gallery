d62d3ca3f5c45f93446461bbdf513d4de70c8eea
Thertor <Thertor@users.noreply.github.com>
2024-08-09 11:11:22 +0000

    deploy: c90e373339622cf135eebf00e048b8e9808606de

---
+++ scripts/slideshow.js
@@ -0,0 +1,28 @@
+function slideshow(products) {
+  const container = document.getElementById('slideshow');
+  let items = ``;
+  slideshowProducts = products.slice(Math.max(products.length - 9, 0)).reverse();
+  slideshowProducts.forEach(function (product, index) {
+    if ((index % 3) == 0) items += '<div class="gallery-image fade">';
+
+    items += `
+      <div class="project-container">
+        <a href="product.html?product=${product.id}">
+          <img
+            src="${product.imageUrl}">
+          <div class="text">${product.category}</div>
+        </a>
+      </div>
+    `;
+
+    if ((index % 3) == 2) items += '</div>';
+  });
+
+  const htmlContent = `
+    ${items}
+  `;
+
+  container.innerHTML = htmlContent;
+}
+
+slideshow(products);
