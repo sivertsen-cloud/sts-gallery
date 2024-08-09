185d2177f5330c9c0f089d824b588c509f05a4a3
Eivind Sivertsen <eivind.sivertsen99@gmail.com>
2024-08-09 17:07:14 +0200

    feat: first transition into npm based build that works

--- src/scripts/head.js
+++ src/scripts/head.js
@@ -6,9 +6,10 @@ document.write('\
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css"> \
     <link href="https://fonts.googleapis.com/css?family=EB Garamond" rel="stylesheet"> \
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet"> \
-    <link rel="stylesheet" href="styles.css"> \
-    <script type="text/javascript" src="data/products.js"></script> \
+    <script type="text/javascript" src="products.bundle.js"></script> \
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js" type="text/javascript"></script> \
     <script src="https://cdn.jsdelivr.net/npm/contentful@latest/dist/contentful.browser.min.js"></script> \
   </head> \
 ');
+
+import '../styles/styles.css';
--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: {
     main: './src/index.js',  // Main entry point
+    products: './src/data/products.js',
     header: './src/scripts/header.js',  // Additional script
     footer: './src/scripts/footer.js',  // Add more scripts as needed
     head: './src/scripts/head.js',
+    productSlide: './src/scripts/productSlide.js',
+    newProduct: './src/scripts/newProduct.js',
+    dots: './src/scripts/dots.js',
+    slideshow: './src/scripts/slideshow.js',
+    gallery: './src/scripts/gallery.js',
+    otherProjects: './src/scripts/otherProjects.js',
   },
   output: {
     filename: '[name].bundle.js',  // Generates separate bundles for each entry
     path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,  // Match all .css files
+        use: ['style-loader', 'css-loader'],  // Process and inject CSS
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -18,40 +33,26 @@ module.exports = {
       filename: 'index.html',
       chunks: ['main'],  // Inject the main bundle into index.html
     }),
+    new HtmlWebpackPlugin({
+      template: './src/gallery.html',
+      filename: 'gallery.html',
+      chunks: ['gallery'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/contact.html',
+      filename: 'contact.html',
+      chunks: ['contact'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/product.html',
+      filename: 'product.html',
+      chunks: ['product'],  // Inject the main bundle into index.html
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/about.html',
+      filename: 'about.html',
+      chunks: ['main'],  // Inject the main bundle into index.html
+    }),
   ],
   mode: 'production',
 };
-
-
-// const path = require('path');
-// const HtmlWebpackPlugin = require('html-webpack-plugin');
-// const glob = require('glob');
-
-// const scriptFiles = glob.sync('./src/scripts/**/*.js');
-// console.log('Script files:', scriptFiles);  // Log the files found by glob
-
-// const entry = {
-//   main: './src/index.js',
-// };
-
-// scriptFiles.forEach(file => {
-//   const name = path.basename(file, path.extname(file)); // Use file name (without extension) as the key
-//   console.log(`Adding entry: ${name} -> ${file}`);  // Log each entry
-//   entry[name] = file;
-// });
-
-// module.exports = {
-//   entry,
-//   output: {
-//     filename: '[name].bundle.js',
-//     path: path.resolve(__dirname, 'dist'),
-//   },
-//   plugins: [
-//     new HtmlWebpackPlugin({
-//       template: './src/index.html',
-//       filename: 'index.html',
-//       chunks: ['main'],
-//     }),
-//   ],
-//   mode: 'production',
-// };
