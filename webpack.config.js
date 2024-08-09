8720f771452d3f2188c3280a55aae7fb7ac24e28
Eivind Sivertsen <eivind.sivertsen99@gmail.com>
2024-08-09 14:29:34 +0200

    feat: this should fix the deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js', // Replace with your entry file
+  entry: './src/index.js',  // Your entry JavaScript file
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'), // Outputs to 'dist' directory
+    path: path.resolve(__dirname, 'dist'),
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',  // Source HTML file
+      filename: 'index.html',        // Output HTML file in the dist folder
+    }),
+  ],
   mode: 'production',
 };
