177192c66f9a4455e7f709493e1afcb3eab6e5dd
Eivind Sivertsen <eivind.sivertsen99@gmail.com>
2024-08-09 14:33:53 +0200

    feat: add scripts into src folder

---
+++ src/scripts/header.js
@@ -0,0 +1,17 @@
+document.write('\
+    <header> \
+      <div class="navigation-bar"> \
+        <div class="logo"> \
+          <a class="logo-link" href="index.html"> \
+            <img src="assets/logo-uten-tekst.png" alt="STS Logo"> \
+            <h2>Sivertsen 3Design</h2> \
+          </a> \
+        </div> \
+        <nav> \
+          <a href="index.html">Hjem</a> \
+          <a href="gallery.html">Galleri</a> \
+          <a href="contact.html">Kontakt</a> \
+        </nav> \
+      </div> \
+    </header> \
+');
