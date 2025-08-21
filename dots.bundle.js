d6b80f89a7434459059dffbf6151ae71e08218a7
Thertor <Thertor@users.noreply.github.com>
2025-08-21 10:55:27 +0000

    deploy: bfa65e9d9e7482f9e8ece0939a0f214a4ec2b474

--- dots.bundle.js
+++
@@ -1,1 +0,0 @@
-!function(n){var c=document.getElementById("slideshowButtons"),s=1,t="";slideshowProducts=n.slice(Math.max(n.length-9,0)).reverse(),slideshowProducts.forEach(function(n,c){c%3==0&&(s>3&&(s=1),t+='\n        <span class="dot" onclick="currentSlide('.concat(s,')"></span>\n      '),s++)});var e="\n    ".concat(t,"\n  ");c.innerHTML=e}(products);
